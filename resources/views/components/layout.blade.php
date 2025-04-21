c9fa81679fa3b54d8ccf8ef5647d74f43d8369d0
devjep <jeffrid.laxamana@gmail.com>
2025-04-21 15:02:10 +0800

    layouting projects part 1

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,16 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pixel Positions</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
-<body>
-        <div>
-            <nav>
-                <div>logo</div>
-                <div>links</div>
-                <div>post a job</div>
+<body class="bg-black/95 text-white ">
+        <div class="px-10">
+            <nav class="flex justify-between items-center py-4 border-b border-white/10">
+                <div>
+                     <a href="">
+                        <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
+                     </a>
+                </div>
+                <div class="space-x-6 font-bold">
+                    <a href="#">Job</a>
+                    <a href="#">Career</a>
+                    <a href="#">Salaries</a>
+                    <a href="#">Companies</a>
+                </div>
+                <div><a href="#">Post a Job</a></div>
             </nav>
 
-            <main>
+            <main class="mt-10 max-w-[986px] mx-auto">
                 {{ $slot }}
             </main>
         </div>
